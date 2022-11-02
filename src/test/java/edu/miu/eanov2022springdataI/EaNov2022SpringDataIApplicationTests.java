431bcd7efd31c95591272471e2c855dd6e7afde3
crisnaxtha <krishnashrestha49@gmail.com>
2022-11-02 14:27:15 -0500

    Inital Commit

---
+++ src/test/java/edu/miu/eanov2022springdataI/EaNov2022SpringDataIApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.eanov2022springdataI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaNov2022SpringDataIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
