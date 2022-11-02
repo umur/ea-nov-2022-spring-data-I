8bd71bbc4c4d8900edbd624e01ae3b33e32e01fa
Md. Siam Biswas <siam.biswas@icloud.com>
2022-11-02 10:50:54 -0700

    Project Setup

---
+++ src/main/java/com/example/eanov2022springdataI/EaNov2022SpringDataIApplication.java
@@ -0,0 +1,13 @@
+package com.example.eanov2022springdataI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaNov2022SpringDataIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaNov2022SpringDataIApplication.class, args);
+	}
+
+}
